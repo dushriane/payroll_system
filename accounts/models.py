1d7a3d2e71d5bfaa891cf17278231954467f50b1
Ishimwe-Marie-Joyeuse <mariejoyeuse26574@gmail.com>
2026-05-08 22:14:34 +0300

    Add custom User model with UserManager for payroll authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, phone_number,  password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, full_name=full_name, phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("is_active", True)
+        return self.create_user(email, full_name, phone_number, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None
+    first_name = None
+    last_name = None
+
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=13, blank=True, null=True)
+    role = models.CharField(max_length=50, 
+                            choices=[('admin', 'Admin'), ('hr', 'HR'), ('employee', 'Employee')],
+                            default='employee')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["full_name", "phone_number"]
+
+    def __str__(self):
+        return self.email
+
+
+
+    
